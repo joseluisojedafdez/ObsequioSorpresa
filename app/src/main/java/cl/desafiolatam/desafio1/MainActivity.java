7d034402045565fefb45d882f5d8b924a821da11
Jose Luis Ojeda <joseluisojedafdez@gmail.com>
2020-11-23 17:01:01 -0300

    Completados todos los pasos y el extra.

--- app/src/main/java/cl/desafiolatam/desafio1/MainActivity.java
+++ app/src/main/java/cl/desafiolatam/desafio1/MainActivity.java
@@ -2,15 +2,15 @@ package cl.desafiolatam.desafio1;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,12 +23,12 @@ public class MainActivity extends AppCompatActivity {
     [ x]6. Implementar un método que haga la entrega de 1 obsequio aleatorio de la lista de obsequios .
     [x ]7. Responder al evento @onClick entregando un obsequio usando el método definido
     anteriormente .
-    [ ]8. Agregar al menos 3 nuevos obsequios a la lista disponible. Estos obsequios no están incluídos
+    [x]8. Agregar al menos 3 nuevos obsequios a la lista disponible. Estos obsequios no están incluídos
     en el proyecto y deben ser agregados usando Vector Asset, desde la galería o descargando
     imágenes desde algún sitio como flaticon.es .
-    [ ] 9. Volver a jugar, agregando un botón debajo de la imagen de resultado que permita reiniciar el
+    [X] 9. Volver a jugar, agregando un botón debajo de la imagen de resultado que permita reiniciar el
     juego usando el vector drawable/ic_replay.xml incluído en el proyecto.
-    [ ] 10. Extra. Cambiar el texto del encabezado según si el premio fue entregado o no. Por ejemplo,
+    [x] 10. Extra. Cambiar el texto del encabezado según si el premio fue entregado o no. Por ejemplo,
     cambiar “Tenemos una sorpresa para ti” por “Felicidades! ganaste un trozo de pizza!” cuando se
     entregue pizza de obsequio. Y volver al mensaje original cuando se reinicie el juego.
 
@@ -38,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imgRight;
     private ImageView imgCenter;
     private ImageView imgResult;
+    private ImageButton replay;
+    private TextView tvIntro;
+    private TextView tvTitle;
+    private Obsequio obsequio;
     private List<Obsequio> listaObsequios=new ArrayList<>();
 
 
@@ -50,24 +54,51 @@ public class MainActivity extends AppCompatActivity {
         imgRight = findViewById(R.id.imageViewRight);
         imgCenter = findViewById(R.id.imageViewCenter);
         imgResult = findViewById(R.id.imageViewResult);
+        replay=findViewById(R.id.imgbt_replay);
+        tvIntro=findViewById(R.id.tv_intro);
+        tvTitle=findViewById(R.id.tv_Title);
 
         imgRight.setOnClickListener(mResultListener);
         imgLeft.setOnClickListener(mResultListener);
         imgCenter.setOnClickListener(mResultListener);
-
+        replay.setOnClickListener(mReplay);
 
     }
 
     private View.OnClickListener mResultListener = new View.OnClickListener() {
         public void onClick(View v) {
             mostrarResultado();
+
         }
     };
 
-        public void mostrarResultado() {
-            Obsequio obsequio = obtenerObsequio();
+    private View.OnClickListener mReplay = new View.OnClickListener() {
+        public void onClick(View v) {
+
+            reiniciarJuego();
+
+        }
+    };
+
+    public void reiniciarJuego(){
+        imgResult.setVisibility(View.INVISIBLE);
+        imgLeft.setVisibility(View.VISIBLE);
+        imgCenter.setVisibility(View.VISIBLE);
+        imgRight.setVisibility(View.VISIBLE);
+        tvIntro.setText("Elige una de estas 3 cajas sorpresa y recibe un obsequio");
+    }
+
+
+    public void mostrarResultado() {
+            obsequio = obtenerObsequio();
             imgResult.setImageResource(obsequio.getImagen());
             imgResult.setVisibility(View.VISIBLE);
+            imgLeft.setVisibility(View.INVISIBLE);
+            imgCenter.setVisibility(View.INVISIBLE);
+            imgRight.setVisibility(View.INVISIBLE);
+            replay.setVisibility(View.VISIBLE);
+            tvIntro.setText("¡¡¡Felicitaciones!!!, has ganado: "+ obsequio.getNombre());
+
         }
 
         public Obsequio obtenerObsequio() {
@@ -86,6 +117,9 @@ public class MainActivity extends AppCompatActivity {
             listaObsequios.add(new Obsequio("Pizza", R.drawable.ic_gift_pizza));
             listaObsequios.add(new Obsequio("Beach", R.drawable.ic_gift_beach));
             listaObsequios.add(new Obsequio("Music", R.drawable.ic_gift_music));
+            listaObsequios.add(new Obsequio("Publica tu propio libro", R.drawable.ic_book));
+            listaObsequios.add(new Obsequio("Cerveza con amigos", R.drawable.ic_cerveza));
+            listaObsequios.add(new Obsequio("Viaje en tiempos de Covid", R.drawable.ic_de_viaje));
 
             return listaObsequios;
 
